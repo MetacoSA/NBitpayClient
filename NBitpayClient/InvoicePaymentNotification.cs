86282a31867e54e1966a4b47aa108984afa46ecb
Andrew Camilleri <evilkukka@gmail.com>
2020-06-01 08:41:27 +0200

    Update InvoicePaymentNotification.cs

--- NBitpayClient/InvoicePaymentNotification.cs
+++ NBitpayClient/InvoicePaymentNotification.cs
@@ -147,7 +147,7 @@ namespace NBitpayClient
 	        get; set;
 	    }
         
-	    [JsonProperty(PropertyName = "_warning")]
+	    [JsonProperty(PropertyName = "_warning", Order = 1)]
 	    public string Warning
 	    {
 	        get { return "This data could have easily been faked and should not be trusted. Please run any invoice checks by first fetching the invoice through the API."; }
